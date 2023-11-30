778b037645e574535faa3c44d8255abcb85bb35f
emilywingler <wingleremily@gmail.com>
2023-11-30 13:41:32 -0300

    Otimização do código

--- Padrao.c
+++ Padrao.c
@@ -9,6 +9,65 @@ typedef struct no
     struct no *prox;
 }Padrao;
 
+
+typedef struct f {
+    void *info;
+    struct f *prox, *ant;
+} fila;
+
+
+fila *Cria(void *info) {
+    fila *novo = (fila *)malloc(sizeof(fila));
+    if (!novo) {
+        printf("Erro de alocação");
+        exit(1);
+    }
+    novo->info = info;
+    novo->prox = NULL;
+    novo->ant = NULL;
+    return novo;
+}
+
+// Entra na fila;
+fila *EntraNaFila(fila *f, void *dado) {
+    fila *novo = Cria(dado);
+    fila *aux = f;
+    
+    while (aux->prox != NULL) {
+        aux = aux->prox;
+    }
+    
+    aux->prox = novo;
+    novo->ant = aux;
+
+    return f;
+}
+
+// Sai da fila
+void SaiDaFila(fila **f) {
+    if ((*f)->prox == NULL) {
+        printf("A fila já está vazia\n");
+        return;
+    }
+
+    fila *aux = (*f)->prox;
+    free(*f);
+    *f = aux;
+    if (*f != NULL) {
+        (*f)->ant = NULL;
+    }
+}
+
+// Tamanho da fila;
+int tamanho(fila *f) {
+    int count = 0;
+    fila *aux = f->prox; // Pular o nó cabeça
+    while (aux != NULL) {
+        count++;
+        aux = aux->prox;
+    }
+    return count;
+}
 // devemos criar um padrão genérico para essa função 
 // porque ela é utilizada por dois arquivos diferentes, Por enquanto deixa assim;
 char *limparPadrao(char *Padrao) {
@@ -24,27 +83,36 @@ char *limparPadrao(char *Padrao) {
     limpaPadrao[j] = '\0';
     return limpaPadrao;
 }
-int main(){
-    FILE *arquivo;
 
-    // Abre o arquivo para leitura
-    arquivo = fopen("PadroesVirus.txt", "r");
-
-    // Verifica se o arquivo foi aberto com sucesso
+void ValidaArquivo(FILE *arquivo){
     if (!arquivo) {
         printf("Não foi possível ler o arquivo\n");
         exit(1);
     }
+}
+
+int FimDeArquivo(char *linha){
+    if(linha[0] == '>' && linha[1] == 'E' && linha[2] == 'O' && linha[3] == 'F'){
+            return 1; //fim de arquivo
+        }
+    return 0; //pode continuar
+}
+int main(){
+    FILE *arquivo;
+    // Abre o arquivo para leitura
+    arquivo = fopen("PadroesVirus.txt", "r");
+    ValidaArquivo(arquivo);
+    // Verifica se o arquivo foi aberto com sucesso
 
     char linha[70];
+    
     Padrao *inicio = NULL;
     Padrao *virus = NULL;
     while (fgets(linha, sizeof(linha), arquivo) != NULL)
     {
         linha[strcspn(linha, "\n")] = '\0';
-        if(linha[0] == '>' && linha[1] == 'E' && linha[2] == 'O' && linha[3] == 'F'){
-            break;
-        }
+    
+        if(FimDeArquivo(linha)) break;
         if (linha[0] == '>') {
             // Alocando memória para o novo nó
             virus = (Padrao *)malloc(sizeof(Padrao));
